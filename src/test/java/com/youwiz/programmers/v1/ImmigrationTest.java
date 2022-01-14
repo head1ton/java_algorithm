7a4e34a6eafb78cde169900ad028453ec7464222
head1ton-dhg <head1ton@v-go.io>
2022-01-14 13:14:38 +0900

    ..

--- src/test/java/com/youwiz/programmers/v1/ImmigrationTest.java
+++ src/test/java/com/youwiz/programmers/v1/ImmigrationTest.java
@@ -12,6 +12,7 @@ public class ImmigrationTest {
     public void immigration0() {
         int n = 6;
         int[] times = {7, 10};
+        int answer = 0;
 
         long max = 0;
         for (int time : times) {
@@ -30,11 +31,47 @@ public class ImmigrationTest {
 
             if (total >= n) {
                 end = mid - 1;
+                answer = (int) mid;
             } else {
                 start = mid + 1;
             }
         }
 
-        System.out.println("answer = " + end);
+        System.out.println("answer = " + answer);
+    }
+
+    @Test
+    public void immgration1() {
+        int n = 6;
+        int[] times = {7, 10};
+        int answer = 0;
+
+        // 최대 시간을 뽑자
+        long max = 0;
+        for (int time : times) {
+            if (max < time) {
+                max = time;
+            }
+        }
+
+        long start = 1, end = max * n;
+
+        while (start <= end) {
+            long mid = (start + end) / 2;
+            long total = 0;
+
+            for (int time : times) {
+                total += mid / time;
+            }
+
+            if (total >= n) {
+                end = mid - 1;
+                answer = (int) mid;
+            } else {
+                start = mid + 1;
+            }
+        }
+        System.out.println("answer = " + answer);
+
     }
 }
